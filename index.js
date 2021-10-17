ffd2f3e4404e6673ea030087ed53850dc8f57cf8
Laurel <millifly@gmail.com>
2021-10-17 00:31:52 -0600

    10.1 10/16 database, /movies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const db = require("./dbConnectExec.js");
+
 const app = express();
 
 app.listen(5000, () => {
@@ -16,3 +18,20 @@ app.get("/", (req, res) => {
 
 // app.post()
 // app.put()
+
+app.get("/movies", (req, res) => {
+  //get data from the database
+  db.executeQuery(
+    `SELECT *
+    FROM movie
+    LEFT JOIN Genre
+    ON genre.GenrePK = movie.GenreFK`
+  )
+    .then((theResults) => {
+      res.status(200).send(theResults);
+    })
+    .catch((myError) => {
+      console.log(myError);
+      res.status(500).send();
+    });
+});
