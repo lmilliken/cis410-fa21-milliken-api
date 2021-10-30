ae3a5aa4cebda8a87d8ac2e1b0e87ec2bba80522
Laurel <millifly@gmail.com>
2021-10-30 11:29:35 -0600

    11.1 11/2 /contacts/log

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const db = require("./dbConnectExec.js");
+const rockwellConfig = require("./config.js");
 
 const app = express();
 app.use(express.json());
@@ -21,45 +23,119 @@ app.get("/", (req, res) => {
 // app.post()
 // app.put()
 
-app.post("/contacts", async (req, res)=>{
-// res.send("/contacts called");
+app.post("/contacts/login", async (req, res) => {
+  // console.log("/contacts/login called", req.body);
 
-// console.log("request body", req.body);
+  //1. data validation
+  let email = req.body.email;
+  let password = req.body.password;
 
-let nameFirst = req.body.nameFirst;
-let nameLast = req.body.nameLast;
-let email = req.body.email;
-let password = req.body.password;
+  if (!email || !password) {
+    return res.status(400).send("Bad request");
+  }
 
-if(!nameFirst || !nameLast || !email || !password){return res.status(400).send("Bad request")};
+  //2. check that user exists in DB
 
-nameFirst = nameFirst.replace("'","''");
-nameLast = nameLast.replace("'","''");
+  let query = `SELECT *
+  FROM Contact
+  WHERE email = '${email}'`;
 
-let emailCheckQuery = `SELECT email
+  let result;
+  try {
+    result = await db.executeQuery(query);
+  } catch (myError) {
+    console.log("error in /contacts/login", myError);
+    return res.status(500).send();
+  }
+
+  // console.log("result", result);
+
+  if (!result[0]) {
+    return res.status(401).send("Invalid user credentials");
+  }
+
+  //3. check password
+  let user = result[0];
+
+  if (!bcrypt.compareSync(password, user.Password)) {
+    console.log("invalid password");
+    return res.status(401).send("Invalid user credentials");
+  }
+
+  //4. generate token
+
+  let token = jwt.sign({ pk: user.ContactPK }, rockwellConfig.JWT, {
+    expiresIn: "60 minutes",
+  });
+  // console.log("token", token);
+
+  //5. save token in DB and send response
+
+  let setTokenQuery = `UPDATE Contact
+  SET token = '${token}'
+  WHERE ContactPK = ${user.ContactPK}`;
+
+  try {
+    await db.executeQuery(setTokenQuery);
+
+    res.status(200).send({
+      token: token,
+      user: {
+        NameFirst: user.NameFirst,
+        NameLast: user.NameLast,
+        Email: user.Email,
+        ContactPK: user.ContactPK,
+      },
+    });
+  } catch (myError) {
+    console.log("error in setting user token", myError);
+    res.status(500).send();
+  }
+});
+
+app.post("/contacts", async (req, res) => {
+  // res.send("/contacts called");
+
+  // console.log("request body", req.body);
+
+  let nameFirst = req.body.nameFirst;
+  let nameLast = req.body.nameLast;
+  let email = req.body.email;
+  let password = req.body.password;
+
+  if (!nameFirst || !nameLast || !email || !password) {
+    return res.status(400).send("Bad request");
+  }
+
+  nameFirst = nameFirst.replace("'", "''");
+  nameLast = nameLast.replace("'", "''");
+
+  let emailCheckQuery = `SELECT email
 FROM contact
 WHERE email = '${email}'`;
 
-let existingUser = await db.executeQuery(emailCheckQuery);
+  let existingUser = await db.executeQuery(emailCheckQuery);
 
-// console.log("existing user", existingUser);
+  // console.log("existing user", existingUser);
 
-if(existingUser[0]){return res.status(409).send("Duplicate email")};
+  if (existingUser[0]) {
+    return res.status(409).send("Duplicate email");
+  }
 
-let hashedPassword = bcrypt.hashSync(password);
+  let hashedPassword = bcrypt.hashSync(password);
 
-let insertQuery = 
-`INSERT INTO contact(NameFirst, NameLast, Email, Password)
+  let insertQuery = `INSERT INTO contact(NameFirst, NameLast, Email, Password)
 VALUES('${nameFirst}','${nameLast}','${email}','${hashedPassword}')`;
 
-db.executeQuery(insertQuery)
-  .then(()=>{res.status(201).send()})
-  .catch((err)=>{
-    console.log("error in POST /contact", err);
-    res.status(500).send();
-  })
-
-})
+  db.executeQuery(insertQuery)
+    .then(() => {
+      res.status(201).send();
+    })
+    .catch((err) => {
+      console.log("error in POST /contact", err);
+      res.status(500).send();
+    });
+});
 
 app.get("/movies", (req, res) => {
   //get data from the database
--- sandbox/hashing.js
+++ sandbox/hashing.js
@@ -1,9 +1,9 @@
 const bcrypt = require("bcryptjs");
 
-let hashedPassword = bcrypt.hashSync('csu123');
+let hashedPassword = bcrypt.hashSync("csu123");
 
 console.log(hashedPassword);
 
-let hashTest = bcrypt.compareSync("csu1234",hashedPassword);
+let hashTest = bcrypt.compareSync("csu123", hashedPassword);
 
-console.log(hashTest)
+console.log(hashTest);
